73e43860249694140fffb0586df37d36d8bfae4a
Aman <amngupta402@gmail.com>
2026-07-10 22:59:52 +0530

    routes

--- src/app/api/documents/route.ts
+++ src/app/api/documents/route.ts
@@ -2,7 +2,8 @@ import prisma from '@/lib/prisma'
 import type { NextRequest } from "next/server";
 import { auth } from "@/lib/auth/auth";
 import { headers } from "next/headers";
+import { requireUser } from '@/lib/auth/helper';
 
 export async function GET(request: NextRequest) {
-
+  const session = await requireUser();
 }
---
+++ src/lib/auth/helper.ts
@@ -0,0 +1,20 @@
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
+
+export async function getSession() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  return session;
+};
+
+export async function requireUser() {
+  const session = await getSession();
+
+  if(!session) {
+    throw new Error("Unauthorized");
+  }
+
+  return session;
+};
